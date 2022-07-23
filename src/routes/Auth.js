31d5bce2b1b02b1ede468d60c23b060f7c870cb1
developersung13 <developersung13@gmail.com>
2022-07-23 18:32:37 +0900

    Update asset

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,29 @@
+import { authService } from "fbase";
 import { useState } from "react";
 
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const onSubmit = (event) => {
+  const [newAccount, setNewAccount] = useState(false);
+  const [error, setError] = useState("");
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message.slice(9, error.message.indexOf(".") + 1));
+    }
   };
   const onChange = (event) => {
     const {
@@ -16,6 +35,7 @@ function Auth() {
       setPassword(value);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -35,8 +55,15 @@ function Auth() {
           required
           onChange={onChange}
         />
-        <input type="submit" value="Sign In" />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign in"}
+        />
+        {error && <span>{error}</span>}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
       </div>
