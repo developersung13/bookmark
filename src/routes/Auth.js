a87e9e67c4ff411409a8b9beeb1430b60c6a6b52
developersung13 <developersung13@gmail.com>
2022-07-23 14:14:01 +0900

    Update asset

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
 function Auth() {
-  return <h1>Auth component</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="password"
+          value={password}
+          required
+          onChange={onChange}
+        />
+        <input type="submit" value="Sign In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
