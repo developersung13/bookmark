c7ef5fdf7c5e7dac95f7bb0e2c53dc9e93548557
developersung13 <developersung13@gmail.com>
2022-08-02 14:09:05 +0900

    Update asset

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "styles.css";
-document.cookie = "safeCookie1=foo; SameSite=Lax";
-document.cookie = "safeCookie2=foo";
-document.cookie = "crossCookie=bar; SameSite=None; Secure";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
