f5e67de64c7e379cc6334feacef333c18bf1b52c
developersung13 <developersung13@gmail.com>
2022-08-16 17:25:08 +0900

    Component update asset

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,20 +1,113 @@
 function Home() {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 mt-7">
       <div>
-        <button>충북대학교 공지사항</button>
-        <button>학부 공지사항</button>
-        <button>SW중심대학사업단 공지사항</button>
-        <button>학생생활관 공지사항</button>
-        <button>1</button>
+        <ul>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.chungbuk.ac.kr/site/www/boardList.do?boardSeq=112&key=698"
+              className="border p-2"
+            >
+              충북대학교
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://software.cbnu.ac.kr/sub0401"
+              className="border p-2"
+            >
+              학부 공지사항
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://sw7up.cbnu.ac.kr/community/notice?page=1&limit=10&sort=-createdAt"
+              className="border p-2"
+            >
+              SW중심대학사업단 공지사항
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://dorm.chungbuk.ac.kr/home/main.php"
+              className="border p-2"
+            >
+              학생생활관 공지사항
+            </a>
+          </li>
+        </ul>
       </div>
       <div>
-        <button>ECampus(블랙보드)</button>
-        <button>씨앗</button>
-        <button>중앙도서관</button>
-        <button>수강신청</button>
-        <button>종합서비스센터 교육과정</button>
-        <button>통합검색</button>
+        <ul>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://ecampus.chungbuk.ac.kr/?new_loc=%2Fultra%2Finstitution-page"
+              className="border p-2"
+            >
+              ECampus(블랙보드)
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://cieat.cbnu.ac.kr/clientMain/a/t/main.do"
+              className="border p-2"
+            >
+              씨앗
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://cbnul.chungbuk.ac.kr/index.ax"
+              className="border p-2"
+            >
+              중앙도서관
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://eisa.cbnu.ac.kr/atlecLogin"
+              className="border p-2"
+            >
+              수강신청
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.chungbuk.ac.kr/site/f09/boardList.do?boardSeq=636&key=1446&part=B00000"
+              className="border p-2"
+            >
+              종합서비스센터 교육과정
+            </a>
+          </li>
+          <li className="m-7">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.chungbuk.ac.kr/wisenut/search.jsp"
+              className="border p-2"
+            >
+              통합검색
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
